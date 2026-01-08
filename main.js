67d4d63d8f60079c43447e0d0a65e2cef82bee9c
yztz8 <timofej.zaitsev@gmail.com>
2026-01-08 14:36:24 +0100

    Implement login

---
+++ main.js
@@ -0,0 +1,18 @@
+let loggedin = false
+  let user = false
+    document.getElementById("login").addEventListener("click",function(){
+      const value = Number(document.getElementById("code").value);
+      const user = document.getElementById("name").value;
+      const au = ["69keyanxtimmygyat67"]
+        const av = [ichgooneauftimundkeyan69]
+          if(av.includes(value) && au.includes(user)){
+            loggedin = true;
+          }
+      if (loggedin==true && av.includes(value) && au.includes(user)){
+        console.log("Gyat")
+          window.location.href = "index.html"
+      }
+      else{
+        console.log("you gay")
+          document.getElementById("wrong").textContent="wrong Info"}
+    });
